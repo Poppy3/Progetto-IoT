a63699786e8a0758ed6686263eee8ee7bb9bdcc1
damianocaprari <damiano.caprari@gmail.com>
2021-03-21 18:01:58 +0100

    sistemate le pagine riguardo i Plant Types, completate funzionalità di list, detail, add, edit, delete.

--- IOT_flowey/server/web/views/plant_type.py
+++ IOT_flowey/server/web/views/plant_type.py
@@ -1,6 +1,6 @@
 from ..forms.plant_type import PlantTypeForm
 from ..models.plant_type import PlantTypeModel, db
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy.exc import OperationalError
 
 
@@ -51,11 +51,20 @@ def details(plant_type_id):
 
     return render_template('plant_type/details.html',
                            title='Plant-Type Details',
-                           plant_type=plant_type)
+                           plant_type_model=plant_type)
+
+
+@plant_type_bp.route('/<int:plant_type_id>/delete')
+def delete(plant_type_id=None):
+    plant_type = PlantTypeModel.query.get_or_404(plant_type_id)
+    db.session.delete(plant_type)
+    db.session.commit()
+
+    return redirect(url_for('plant_type.list_all'))
 
 
 @plant_type_bp.route('/create', methods=['GET', 'POST'])
-def create_form():
+def create():
     model = PlantTypeModel()
     form = PlantTypeForm(obj=model)
     error = None
@@ -73,18 +82,17 @@ def create_form():
             db.session.commit()
             success = 'Successfully created a new plant type.'
 
-    return render_template('plant_type/forms/create.html',
+    return render_template('plant_type/form.html',
                            title='Create Plant-Type',
                            plant_type_form=form,
-                           form_endpoint={'endpoint': 'plant_type.create_form'},
+                           form_endpoint={'endpoint': 'plant_type.create'},
                            back_endpoint={'endpoint': 'plant_type.list_all'},
                            error=error,
-                           success=success,
-                           mode='CREATE')
+                           success=success)
 
 
 @plant_type_bp.route('/<int:plant_type_id>/edit', methods=['GET', 'POST'])
-def edit_form(plant_type_id):
+def edit(plant_type_id):
     plant_type = PlantTypeModel.query.get_or_404(plant_type_id)
     form = PlantTypeForm(obj=plant_type)
     error = None
@@ -100,13 +108,13 @@ def edit_form(plant_type_id):
             db.session.commit()
             success = 'Successfully edited the plant type.'
 
-    return render_template('plant_type/forms/create.html',
+    return render_template('plant_type/form_edit.html',
                            title='Edit Plant-Type',
+                           plant_type_model=plant_type,
                            plant_type_form=form,
-                           form_endpoint={'endpoint': 'plant_type.edit_form',
+                           form_endpoint={'endpoint': 'plant_type.edit',
                                           'plant_type_id': plant_type_id},
                            back_endpoint={'endpoint': 'plant_type.details',
                                           'plant_type_id': plant_type_id},
                            error=error,
-                           success=success,
-                           mode='EDIT')
+                           success=success)
