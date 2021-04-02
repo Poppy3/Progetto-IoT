fef6858ad21487f0e53f52e8026e51990cac32c7
damianocaprari <damiano.caprari@gmail.com>
2021-04-02 14:41:22 +0200

    aggiunti parametri settaggio all'engine di sqlalchemy
    dovrebbero permettere di ridurre l'occorrenza di errori dovuti a connessioni scadute col db (che si verificano con l'app hostata su pythonanywhere)

--- IOT_flowey/server/web/config.py
+++ IOT_flowey/server/web/config.py
@@ -12,6 +12,10 @@ TEMPLATES_AUTO_RELOAD = True
 # SQLALCHEMY_DATABASE_URI = 'mysql://sql11398075:X3LHKv2R4k@sql11.freemysqlhosting.net/sql11398075'
 # Database locale
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'dev.sqlite3')
+SQLALCHEMY_ENGINE_OPTIONS = {
+    "pool_pre_ping": True,
+    "pool_recycle": 250,
+}
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 RESTFUL_PREFIX = '/api/v1'
