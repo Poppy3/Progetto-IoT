81393dcef2567ce0b9e4c2646cc7c7c53933e995
Asia <asia.b@outlook.com>
2021-04-13 00:47:47 +0200

    aggiunta possibilità di filtrare lista di plant_data in base a 'plant_id' eo 'bridge_id' se forniti come querystring

--- IOT_flowey/server/web/views/graphs.py
+++ IOT_flowey/server/web/views/graphs.py
@@ -3,6 +3,7 @@ from ..models.plant_type import PlantTypeModel, db
 from ..models.plant_data import PlantDataModel, db
 from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy.exc import OperationalError
+import datetime
 
 
 graphs_bp = Blueprint('graphs', __name__, url_prefix='/graphs')
@@ -23,19 +24,45 @@ def list_all():
 
 @graphs_bp.route('/<plant_id>')
 def details(plant_id):
+    labels = []
+    values = []
+    title = 'Measurements of plant id:' + plant_id
     plant_measurements = PlantDataModel.query.filter_by(plant_id=plant_id).all()
-    print(plant_measurements)
-    return render_template('graphs/details.html',
-                           title=plant_id,
-                           plant_data=plant_measurements)
+    for item in plant_measurements:
+        labels.append(item.creation_date)
+        values.append(item.humidity_1)
+        type_id=item.plant_type_id
+
+    plant_type = PlantTypeModel.query.filter_by(id=type_id).first()
+    plant_type.humidity_min = 1230
+    plant_type.humidity_max = 1240
+    print (plant_type)
+
+    return render_template('graphs/test2.html',
+                           values=values,
+                           labels=labels,
+                           title=title,
+                           plant_type=plant_type)
 
 
 @graphs_bp.route('/test')
 def test():
     legend = 'Monthly Data'
-    labels = ["January", "February", "March", "April", "May", "June", "July", "August"]
-    values = [10, 9, 8, 7, 6, 4, 7, 8]
-    return render_template('graphs/test2.html',
+
+    labels = []
+    values = []
+    plant_measurements = PlantDataModel.query.filter_by(plant_id='fffa776abc9f47568e943904ce792d4b').all()
+    for item in plant_measurements:
+        #labels.append(item. creation_date.strftime("%d/%m - %H:%M"))
+        labels.append(item.creation_date)
+        values.append(item.humidity_1)
+    #labels.append(datetime.datetime(2020, 8, 13, 23, 40, 11, 99399))
+    #values.append(1234.1)
+    labels.append(datetime.datetime(2021, 3, 13, 23, 50, 20, 99399))
+    values.append(1234.1)
+    labels.append(datetime.datetime(2021, 3, 13, 23, 50, 40, 99399))
+    values.append(1234.0)
+    return render_template('graphs/test.html',
                            values=values,
                            labels=labels,
                            legend=legend)
