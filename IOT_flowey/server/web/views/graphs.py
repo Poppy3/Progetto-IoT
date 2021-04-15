d71d27f8edaafbcd3bc1e3d988d494fe72f52361
Asia <asia.b@outlook.com>
2021-04-15 12:34:32 +0200

    aggiunta possibilità di filtrare lista di plant_data in base a 'plant_id' eo 'bridge_id' se forniti come querystring

--- IOT_flowey/server/web/views/graphs.py
+++ IOT_flowey/server/web/views/graphs.py
@@ -13,8 +13,6 @@ graphs_bp = Blueprint('graphs', __name__, url_prefix='/graphs')
 def list_all():
     try:
         plants = PlantDataModel.query.with_entities(PlantDataModel.plant_id).distinct().all()
-        print("HEREEEEEEE")
-        print(plants)
     except OperationalError:
         plants = None
 
@@ -24,40 +22,71 @@ def list_all():
 
 @graphs_bp.route('/<plant_id>')
 def details(plant_id):
+    try:
+        plant_measurements = PlantDataModel.query.filter_by(plant_id=plant_id).all()
+    except OperationalError:
+        plant_measurements = None
     labels = []
-    values = []
+    data = []
+    for i in range(8):
+        data.append([])
     title = 'Measurements of plant id:' + plant_id
-    plant_measurements = PlantDataModel.query.filter_by(plant_id=plant_id).all()
     for item in plant_measurements:
         labels.append(item.creation_date)
-        values.append(item.humidity_1)
-        type_id=item.plant_type_id
+        data[0].append(item.dht_humidity)
+        data[1].append(item.dht_temperature)
+        data[2].append(item.humidity_1)
+        data[3].append(item.humidity_2)
+        data[4].append(item.humidity_3)
+        data[5].append(item.luminosity_1)
+        data[6].append(item.luminosity_2)
+        data[7].append(item.temperature)
 
-    plant_type = PlantTypeModel.query.filter_by(id=type_id).first()
-    plant_type.humidity_min = 1230
-    plant_type.humidity_max = 1240
-    print (plant_type)
+    plant_info = plant_measurements[0]
+
+    type_id = plant_measurements[0].plant_type_id
+    try:
+        plant_type = PlantTypeModel.query.filter_by(id=type_id).first()
+    except OperationalError:
+        plant_type = None
+
+    #plant_type.humidity_max=70.0
+    #plant_type.humidity_min=30.0
 
     return render_template('graphs/details.html',
-                           values=values,
+                           values=data,
                            labels=labels,
                            title=title,
-                           plant_type=plant_type)
+                           plant_type=plant_type,
+                           plant_info = plant_info)
 
 
 @graphs_bp.route('/test')
 def test():
     labels = []
+    data = []
+    for i in range(8):
+        data.append([])
     title = 'Measurements of plant id:'# + plant_id
     plant_measurements = PlantDataModel.query.filter_by(plant_id='442217262fbc447bbab8f677f4a50fd1').all()
     for item in plant_measurements:
         labels.append(item.creation_date)
-        type_id=item.plant_type_id
+        data[0].append(item.dht_humidity)
+        data[1].append(item.dht_temperature)
+        data[2].append(item.humidity_1)
+        data[3].append(item.humidity_2)
+        data[4].append(item.humidity_3)
+        data[5].append(item.luminosity_1)
+        data[6].append(item.luminosity_2)
+        data[7].append(item.temperature)
+    type_id = plant_measurements[0].plant_type_id
     plant_type = PlantTypeModel.query.filter_by(id=type_id).first()
+    plant_info = PlantDataModel.query.filter_by(plant_id='442217262fbc447bbab8f677f4a50fd1').first()
 
     return render_template('graphs/test2.html',
-                           data=plant_measurements,
+                           values=data,
                            labels=labels,
                            plant_type=plant_type,
-                           title=title)
+                           title=title,
+                           plant_info=plant_info)
 
