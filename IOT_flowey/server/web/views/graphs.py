2079998cf657243ec28c683c9ccaee1b90038cf1
Asia <asia.b@outlook.com>
2021-04-21 17:26:38 +0200

    Finite le pagine di umidità temperatura e luminosità.

--- IOT_flowey/server/web/views/graphs.py
+++ IOT_flowey/server/web/views/graphs.py
@@ -62,12 +62,106 @@ def details(plant_id):
                            plant_info = plant_info)
 
 
+@graphs_bp.route('/humidity')
+def humidity_graph():
+    try:
+        plant_measurements = PlantDataModel.query.order_by(PlantDataModel.creation_date).all()
+    except OperationalError:
+        plant_measurements = None
+    try:
+        plants_count = PlantDataModel.query.with_entities(PlantDataModel.plant_id).distinct().count()
+    except OperationalError:
+        plants_count = None
+
+    labels=[]
+    data =[]
+    plants = PlantDataModel.query.group_by(PlantDataModel.plant_id).all()
+
+    for item in plants:
+        data.append([item.plant_id, item.gateway_id, item.bridge_id, [], []]) #last 2 are dht and mean
+
+    for plant_item in plant_measurements:
+        labels.append(plant_item.creation_date)
+        for data_item in data:
+            if data_item[0]==plant_item.plant_id:
+                data_item[3].append(plant_item.dht_humidity)
+                data_item[4].append((plant_item.humidity_1 + plant_item.humidity_2 + plant_item.humidity_3)/3)
+            else:
+                data_item[3].append('null')
+                data_item[4].append('null')
+
+    return render_template('graphs/single_measurement.html',
+                           labels=labels,
+                           data=data,
+                           measure="Humidity")
+
+@graphs_bp.route('/temperature')
+def temperature_graph():
+    try:
+        plant_measurements = PlantDataModel.query.order_by(PlantDataModel.creation_date).all()
+    except OperationalError:
+        plant_measurements = None
+
+    labels=[]
+    data =[]
+    plants = PlantDataModel.query.group_by(PlantDataModel.plant_id).all()
+
+    for item in plants:
+        data.append([item.plant_id, item.gateway_id, item.bridge_id, [], []]) #last 2 are dht and mean
+
+    for plant_item in plant_measurements:
+        labels.append(plant_item.creation_date)
+        for data_item in data:
+            if data_item[0]==plant_item.plant_id:
+                data_item[3].append(plant_item.dht_temperature)
+                data_item[4].append(plant_item.temperature)
+            else:
+                data_item[3].append('null')
+                data_item[4].append('null')
+
+    return render_template('graphs/single_measurement.html',
+                           labels=labels,
+                           data=data,
+                           measure="Temperature")
+
+
+@graphs_bp.route('/luminosity')
+def luminosity_graph():
+    try:
+        plant_measurements = PlantDataModel.query.order_by(PlantDataModel.creation_date).all()
+    except OperationalError:
+        plant_measurements = None
+
+    labels=[]
+    data =[]
+    plants = PlantDataModel.query.group_by(PlantDataModel.plant_id).all()
+
+    for item in plants:
+        data.append([item.plant_id, item.gateway_id, item.bridge_id, []]) #last 2 are dht and mean
+
+    for plant_item in plant_measurements:
+        labels.append(plant_item.creation_date)
+        for data_item in data:
+            if data_item[0]==plant_item.plant_id:
+                data_item[3].append((plant_item.luminosity_1 + plant_item.luminosity_2 )/2)
+            else:
+                data_item[3].append('null')
+
+    return render_template('graphs/single_measurement.html',
+                           labels=labels,
+                           data=data,
+                           measure="Luminosity")
+
+
+#----- json functions to work with a local json file stored in the views folder
+
 @graphs_bp.route('/json/<plant_id>')
 def details_json(plant_id):
     p = Path(__file__).with_name('plant_data.json')
 
     with p.open('r') as f:
         plant_measurements = json.load(f)
+        plant_measurements.sort(key = lambda x:x['creation_date'])
         labels = []
         data = []
         print(plant_measurements)
@@ -107,6 +201,7 @@ def details_json(plant_id):
                                plant_info=plant_info)
 
 
+
 @graphs_bp.route('/json/humidity')
 def humidity_graph_json():
     p = Path(__file__).with_name('plant_data.json')
@@ -116,6 +211,7 @@ def humidity_graph_json():
 
     data=[]
     labels=[]
+    plant_measurements.sort(key=lambda x: x['creation_date'])
     data.append(["43c311e2279f4dd2afce8bbcff62371d", "ARDUINO001", "RASPBERRY001", [], []]) #last 2 are dht and mean
     data.append(["c5a3b03d82944886bb1fdb30612d5c5d", "ARDUINO001","RASPBERRY001", [], []])  # last 2 are dht and mean
 
@@ -129,38 +225,63 @@ def humidity_graph_json():
                 data_item[3].append('null')
                 data_item[4].append('null')
 
-    return render_template('graphs/humidity.html',
+    return render_template('graphs/single_measurement.html',
                            labels=labels,
-                           data=data)
+                           data=data,
+                           measure="Humidity")
 
-@graphs_bp.route('/humidity')
-def humidity_graph():
-    try:
-        plant_measurements = PlantDataModel.query.order_by(PlantDataModel.creation_date).all()
-    except OperationalError:
-        plant_measurements = None
-    try:
-        plants_count = PlantDataModel.query.with_entities(PlantDataModel.plant_id).distinct().count()
-    except OperationalError:
-        plants_count = None
 
+@graphs_bp.route('/json/temperature')
+def temperature_graph_json():
+    p = Path(__file__).with_name('plant_data.json')
+
+    with p.open('r') as f:
+        plant_measurements = json.load(f)
+
+    data=[]
     labels=[]
-    data =[]
-    plants = PlantDataModel.query.group_by(PlantDataModel.plant_id).all()
-
-    for item in plants:
-        data.append([item.plant_id, item.gateway_id, item.bridge_id, [], []]) #last 2 are dht and mean
+    plant_measurements.sort(key=lambda x: x['creation_date'])
+    data.append(["43c311e2279f4dd2afce8bbcff62371d", "ARDUINO001", "RASPBERRY001", [], []]) #last 2 are dht and mean
+    data.append(["c5a3b03d82944886bb1fdb30612d5c5d", "ARDUINO001","RASPBERRY001", [], []])  # last 2 are dht and mean
 
     for plant_item in plant_measurements:
-        labels.append(plant_item.creation_date)
+        labels.append(plant_item["creation_date"])
         for data_item in data:
-            if data_item[0]==plant_item.plant_id:
-                data_item[3].append(plant_item.dht_humidity)
-                data_item[4].append((plant_item.humidity_1 + plant_item.humidity_2 + plant_item.humidity_3)/3)
+            if data_item[0]==plant_item["plant_id"]:
+                data_item[3].append(plant_item["dht_temperature"])
+                data_item[4].append(plant_item["temperature"])
             else:
                 data_item[3].append('null')
                 data_item[4].append('null')
 
-    return render_template('graphs/humidity.html',
+    return render_template('graphs/single_measurement.html',
                            labels=labels,
-                           data=data)
+                           data=data,
+                           measure="Temperature")
+
+
+@graphs_bp.route('/json/luminosity')
+def luminosity_graph_json():
+    p = Path(__file__).with_name('plant_data.json')
+
+    with p.open('r') as f:
+        plant_measurements = json.load(f)
+
+    data=[]
+    labels=[]
+    plant_measurements.sort(key=lambda x: x['creation_date'])
+    data.append(["43c311e2279f4dd2afce8bbcff62371d", "ARDUINO001", "RASPBERRY001", []])
+    data.append(["c5a3b03d82944886bb1fdb30612d5c5d", "ARDUINO001","RASPBERRY001", []])
+
+    for plant_item in plant_measurements:
+        labels.append(plant_item["creation_date"])
+        for data_item in data:
+            if data_item[0]==plant_item["plant_id"]:
+                data_item[3].append((plant_item["luminosity_1"] + plant_item["luminosity_2"])/2)
+            else:
+                data_item[3].append('null')
+
+    return render_template('graphs/single_measurement.html',
+                           labels=labels,
+                           data=data,
+                           measure="Luminosity")
